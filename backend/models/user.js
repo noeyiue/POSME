2343a7c936758a90c4a884b926f7f3f2f860839a
Knet_remote <kantathorn.w@ku.th>
2022-08-30 04:42:03 +0000

    RM not use folder

---
+++ backend/models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const passportLocalMongoose = require('passport-local-mongoose')
+
+
+// User Schema
+const UserSchema = new Schema({
+	username: {type: String, required:true, unique:true},
+	store_name: {type: String, required: true},
+	address: {type: String, required: true},
+	f_name: {type: String, required: true},
+	l_name: {type: String, required: true},
+	email: {type: String, required: true},
+	promptpay_number: {type: String, required: true}
+})
+
+
+UserSchema.plugin(passportLocalMongoose)
+
+
+module.exports = mongoose.model("User", UserSchema)
---
+++ backend/routes/auth.js
@@ -0,0 +1,66 @@
+const express = require('express')  
+const passport = require('passport')
+const User = require('../models/user')
+
+const router = express.Router()
+
+
+router.post('/register', (req, res, next) => {
+	const { username, password, store_name, address, f_name, l_name, email, promptpay_number } = req.body;
+	const this_user = new User({
+		username: username,
+		store_name: store_name,
+		address: address,
+		f_name: f_name,
+		l_name: l_name,
+		email: email,
+		promptpay_number: promptpay_number
+	})
+	User.register(this_user, password, (err) => {
+		console.log('check!')
+		if (err) {
+			res.status(400).json(err)
+		}
+		res.status(200).json({'message': 'successfully created account'})
+		
+	})
+})
+
+
+router.post('/login', passport.authenticate('local'), (req, res) => {
+	res.status(200).json({'message': 'successfully login'})
+})
+
+
+router.post('/logout', (req, res) => {
+	req.logout((err) => {
+		if (err) {
+			res.status(400).json(err)
+		}
+		res.status(200).json({'message': 'successfully logged-out'})
+	})
+})
+
+
+function isLoggedIn(req, res, next) {
+	if (!req.isAuthenticated()) {
+		res.status(400).json({'message': 'not logged-in'})
+	} else {
+		return next()
+	}
+}
+
+
+router.get('/is_logged_in_check', (req, res, next) => {
+	isLoggedIn(req, res, next)
+	res.status(200).json({'message': `logged-in as (${req.user.username})`})
+})
+
+
+module.exports = router
+
+
+
+
+
+
