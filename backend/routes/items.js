035f0e0c7db0dadc3aec6b3cd0d73d5414e64843
PhumipatPeamlue <107468576+PhumipatPeamlue@users.noreply.github.com>
2022-09-08 18:25:15 +0700

    แก้ add item ให้เพิ่มพวก itemtype และ itemoption จัดการเรื่อง key ที่เก็บที่ต่างๆ

--- backend/routes/items.js
+++ backend/routes/items.js
@@ -4,36 +4,137 @@ const { isLoggedIn } = require('./auth')
 
 const User = require('../models/user')
 const Item = require('../models/item')
+const ItemType = require('../models/item_type')
+const ItemOption = require('../models/item_option')
 
 router.use(isLoggedIn)
 
 
 // add item
-router.post('/', (req, res, next) => {
+router.post('/', (req, res) => {
+	const { barcode, name, price, description, type, options, discount } = req.body;
+
 	const item = new Item({
-		barcode: req.body.barcode,
-		name: req.body.name,
-		price: req.body.price,
-		description: req.body.description
-	})
-	item.save((err) => {
+		barcode: barcode,
+		name: name,
+		price: price,
+		description: description,
+		discount: discount
+	});
+
+	// เช็ค ItemType ถ้ามีอยู่ใน collection แล้ว ให้ไปดึง _id ของ type นั้น, ถ้าไม่มีให้สร้าง type ใหม่
+	ItemType.find().where('_id').in(req.user.item_type).exec((err, itemTypeArray) => { // เช็คว่า user มี itemType อะไรบ้าง
 		if (err) {
-			res.status(400).json(err)
+			res.status(400).json({1.: err});
 		}
-		User.findByIdAndUpdate(req.user._id, 
-			{$push: {item: item._id}},
-	    	{safe: true, upsert: true},
-	    	(err, suc) => {
-	    		if (err) {
-	    			console.log('error')
-	    		} else {
-	    			console.log('success')
-	    		}
-	    	}
-    	)
-    	res.end()
-	})
-})
+		let found = false;
+		for (let i = 0; i < itemTypeArray.length; i++){
+			if (itemTypeArray[i].type_name === type) {
+				found = true;
+				Item.findByIdAndUpdate(
+					item._id,
+					{ type: itemTypeArray[i]._id },
+					(err) => {
+						if (err) {
+							res.status(400).json({2.: err});
+						}
+					}
+				);
+				break;
+			}
+		}
+		if (!found) { // สร้าง type ใหม่
+			const itemType = new ItemType({
+				type_name: type
+			});
+			itemType.save((err) => {
+				if (err) {
+					res.status(400).json({3: err});
+				}
+				User.findByIdAndUpdate( // add new itemType in user
+					req.user._id,
+					{$push: {item_type: itemType._id}},
+					{safe: true, upsert: true},
+					(err) => {
+						if (err) {
+							res.status(400).json({4.: err});
+						}
+					}
+				);
+			});
+			Item.findByIdAndUpdate( // set type in item
+				item._id,
+				{ type: itemType._id },
+				(err) => {
+					if (err) {
+						res.status(400).json({5.: err});
+					}
+				}
+			)
+		}
+	});
+
+	// option
+	if (options !== undefined && options.length !== 0) {
+		options.forEach(option => {
+			ItemOption.findOne({option_name: option}, (err, itemOpiton) => {
+				if (err) {
+					res.status(400).json({6.: err});
+				}
+				if (itemOpiton) {
+					Item.findByIdAndUpdate(
+						item._id,
+						{$push: {option: itemOpiton._id}},
+						{safe: true, upsert: true},
+						(err) => {
+							if (err) {
+								res.status(400).json({7.: err});
+							}
+						}
+					);
+				} else {
+					const newItemOption = new ItemOption({
+						option_name: option
+					});
+					newItemOption.save((err) => {
+						if (err) {
+							res.status(400).json({8.: err});
+						}
+						Item.findByIdAndUpdate(
+							item._id,
+							{$push: {option: newItemOption._id}},
+							{safe: true, upsert: true},
+							(err) => {
+								if (err) {
+									res.status(400).json({9.: err});
+								}
+							}
+						);
+					});
+				}
+			})
+		});
+	}
+
+	item.save((err) => {
+    if (err) {
+      res.status(400).json({10.: err});
+    } else {
+      User.findByIdAndUpdate(
+        req.user._id,
+        {$push: {item: item._id}},
+        {safe: true, upsert: true},
+        (err) => {
+          if (err) {
+            res.status(400).json({11.: err});
+          } else {
+            res.status(200).json({message: 'add new item to database complete!'});
+          }
+        }
+      );
+    }
+  });
+});
 
 
 // get item list
